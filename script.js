3e00cfa5944998c04ad9ce9cf496763355ffb06b
Brenda Prado <gabrie221128@gmail.com>
2025-11-01 00:31:55 -0300

    Adicionando a troca de imagem e também animação ao trocar a imagem

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-var circulo = document.querySelector(".circulo")
+const circulo = document.querySelector(".circulo");
+const imagem = document.getElementById("img");
 
-function mudarCor(cor){
-    console.log(cor)
-    circulo.style.backgroundColor = cor
+// Função que muda a cor do círculo e a imagem principal
+function mudarCor(cor, novaImagem) {
+  circulo.style.backgroundColor = cor;
+  imagem.classList.remove("animate"); // remove pra poder reaplicar depois
+
+  setTimeout(() => {
+    imagem.src = novaImagem;
+    imagem.classList.add("animate"); // reaplica a animação
+  }, 50);
 }
